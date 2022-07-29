b9b874cd4afd10d7e1eff402ab1013b363dbe609
cebarrico <cbarrico@gmail.com>
2022-07-29 12:38:36 -0300

    teste mobile

--- js/script.js
+++ js/script.js
@@ -1,36 +1,37 @@
-const mario = document.querySelector('.mario')
-const pipe = document.querySelector('.pipe')
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 
+const btnMobile = document.querySelector("botaomobile");
 
-const jump = () => { 
-    mario.classList.add(`jump`);
+const jump = () => {
+  mario.classList.add(`jump`);
 
-    setTimeout(()=> {
-        mario.classList.remove('jump');
-
-    }, 500)
-}
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+};
 
 const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 97) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
 
-    if (pipePosition <= 120  && pipePosition > 0 && marioPosition < 97 ) {
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
 
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`
+    mario.src = "./img/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "50px";
 
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
-
-        mario.src= './img/game-over.png';
-        mario.style.width = '75px'
-        mario.style.marginLeft = '50px'
-
-        clearInterval(loop);
-    }
-    
+    clearInterval(loop);
+  }
 }, 10);
 
-document.addEventListener (`keydown`, jump);
+document.addEventListener(`keydown`, jump);
+document.addEventListener("click", jump);
+document.addEventListener("touchmove", jump);
